fae7a12192511cb159d1ec847cff4861c0a939ea
Jeremy J Starcher <jeremy.starcher@gmail.com>
2023-08-12 01:11:00 -0400

    Moved config settings directly to the config object rather than using a useless DTO

--- py/atari_a8.py
+++ py/atari_a8.py
@@ -4,42 +4,24 @@ from process_keyboard import ProcessConfiguration
 from process_keyboard import ProcessKeyboard
 
 
-@dataclass
-class KeyboardPaths:
-    base_path: Path
-
-    def __post_init__(self):
-        self.qmk_layout = self.base_path / "qmkinfo.json"
-        self.raw_layout = self.base_path / "raw-layout-autogen.raw"
-        self.pcb = self.base_path / "kicad" / "atari-keyboard.kicad_pcb"
-        self.schematic = self.base_path / "kicad" / "keyboard.kicad_sch"
-        self.openscad_position = self.base_path / "keycaps" / "keyboard-position.scad"
-        self.case_position = self.base_path / "case" / "case-position.scad"
-        self.jlc_bom = self.base_path / "kicad" / "gerbers" / "jlc_bom.csv"
-        self.jlc_cpl = self.base_path / "kicad" / "gerbers" / "jlc_cpl.csv"
-        self.kicad_3dmodel = Path("..") / ".." / "kicad-lib" / "3d-models"
-        self.kicad_keycap_vrml = Path("..") / "keycaps" / "vrml"
-        self.json_path_to_qmk_layout = "layouts.LAYOUT.layout"
-
-
 if __name__ == "__main__":
     base_path = Path("../keyboards/atari-a8")
-    paths = KeyboardPaths(base_path)
 
     config = ProcessConfiguration()
-    config.qmk_layout_filename = paths.qmk_layout
-    config.raw_layout_filename = paths.raw_layout
-    config.pcb_filename = paths.pcb
-    config.keyboard_sch_sheet_filename_name = paths.schematic
-    config.openscad_position_filename = paths.openscad_position
-    config.case_filename = paths.case_position
-    config.jlc_bom_filename = paths.jlc_bom
-    config.jlc_cpl_filename = paths.jlc_cpl
-    config.matrix_starting_index = 201
-    config.kicad_3dmodel_path_str = str(paths.kicad_3dmodel)
-    config.kicad_keycap_vrml_path_str = str(paths.kicad_keycap_vrml)
+    config.qmk_layout_filename = base_path / "qmkinfo.json"
+    config.raw_layout_filename = base_path / "raw-layout-autogen.raw"
+    config.pcb_filename = base_path / "kicad" / "atari-keyboard.kicad_pcb"
+    config.keyboard_sch_sheet_filename_name = base_path / "kicad" / "keyboard.kicad_sch"
+    config.openscad_position_filename = base_path / "keycaps" / "keyboard-position.scad"
+    config.case_filename = base_path / "case" / "case-position.scad"
+    config.jlc_bom_filename = base_path / "kicad" / "gerbers" / "jlc_bom.csv"
+    config.jlc_cpl_filename = base_path / "kicad" / "gerbers" / "jlc_cpl.csv"
 
-    config.json_path_to_qmk_layout = paths.json_path_to_qmk_layout
+    config.kicad_3dmodel_path_str = str(Path("..") / ".." / "kicad-lib" / "3d-models")
+    config.kicad_keycap_vrml_path_str = str(Path("..") / "keycaps" / "vrml")
+    config.json_path_to_qmk_layout = "layouts.LAYOUT.layout"
+
+    config.matrix_starting_index = 201
 
     process = ProcessKeyboard(config)
 
